92164c04b9e05015b0b4073fa7109b6f69581697
Jia <jiafeng3124@gmail.com>
2019-03-04 12:22:58 -0800

    Added GroceryListItem component

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-var GroceryList = () => (
+var GroceryList = (props) => (
   <ul>
-  <h2>Grocery List</h2>
-    <Apples />
-    <Pears />
+    <li>{props.item[0]}</li>
+    <li>{props.item[1]}</li>
+    <li>{props.item[2]}</li>
   </ul>
 )
 
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+var GroceryListItem = () => (
+  <div>
+  <h2>Grocery List</h2>
+  <GroceryList item={['apples', 'pears', 'strawberries']} />
+  </div>
+)
+
+ReactDOM.render(<GroceryListItem />, document.getElementById("app"));
+
+// var TodoList = (props) => (
+//   <ul>
+//     <li>{props.todos[0]}</li>
+//     <li>{props.todos[1]}</li>
+//     <li>{props.todos[2]}</li>
+//   </ul>
+// );
+
+// var App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
+//   </div>
+// );
+
+// ReactDOM.render(<App />, document.getElementById("app"));
